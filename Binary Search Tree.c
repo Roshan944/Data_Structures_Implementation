0b85822c31d2fadbd6c187ad40d37d365dfa3969
Roshan944 <94157286+Roshan944@users.noreply.github.com>
2022-11-28 12:08:05 +0530

    Add files via upload

---
+++ Binary Search Tree.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct tnode* nn;
+int opt,x;
+nn T=NULL;
+struct tnode{
+    nn left;
+    int data;
+    nn right;
+};
+nn insert(nn T,int x){
+    if(T==NULL) 
+    {
+        nn T =malloc(sizeof(struct tnode));
+        if(T!=NULL)
+        {
+        T->data=x;
+        T->left=NULL;
+        T->right=NULL;
+       // printf("%d is inserted",x);
+            return T;
+        }
+    }
+    else
+    {
+        if(x<T->data)
+        {
+            T->left=insert(T->left,x);
+          //  printf("%d is inserted in left",x);
+                return T;
+        }
+        if(x>T->data)
+        {
+           T->right=insert(T->right,x);
+       // printf("%d is inserted in right",x);
+            return T;
+        }
+
+    }
+
+    return T;
+}
+nn makeEmpty(nn T){
+    if(T!=NULL)
+    {
+    makeEmpty(T->left);
+    makeEmpty(T->right);
+    free(T);
+    }
+     printf("Tree is empty");
+    return NULL;
+}
+void inorder(nn T){
+    if(T!=NULL)
+    {
+    inorder(T->left);
+    printf("%d\t",T->data);
+    inorder(T->right);
+    }
+}
+void preorder(nn T){
+    if(T!=NULL){
+        printf("%d\t",T->data);
+        preorder(T->left);
+        preorder(T->right);
+    }
+}
+void postorder(nn T){
+    if(T!=NULL){
+    postorder(T->left);
+    postorder(T->right);
+    printf("%d\t",T->data);
+    }
+}
+
+void main(){
+    while(1)
+    {
+    mainMenu:
+    printf("\nBinary Search Tree\n1.Insert\n2.Inorder\n3.MakeEmpty\n4.Preorder\n5.Postorder\n6.Exit\nEnter the option:");
+    scanf("%d",&opt);
+    switch(opt){
+    case 1:
+    printf("\nEnter the element:");
+    scanf("%d",&x);
+    T=insert(T,x);
+    break;
+    case 2:
+    inorder(T);
+    break;
+    case 3:
+    makeEmpty(T);
+    break;
+    case 4:
+    preorder(T);
+    break;
+    case 5:
+    postorder(T);
+    break;
+    case 6:
+    exit(0);
+    break;
+    default:
+    printf("Invalid");
+    }
+    }
+}
